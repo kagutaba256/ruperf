7ce2327e4ce93a1e1c75196ce7cbb3e31f220c00
Sam Little <kagutaba256@gmail.com>
2021-08-12 15:37:35 -0700

    Feat: `ioctl, read` tests

--- src/test/fd.rs
+++ src/test/fd.rs
@@ -46,11 +46,51 @@ pub fn test_fd() -> Test {
         }
     }
 
+    fn test_read() -> Test {
+        fn check_read(settings: &RunSettings) -> TestResult {
+            use libc::{ioctl, read};
+            let event = &mut perf_event_attr {
+                type_: perf_type_id_PERF_TYPE_HARDWARE,
+                size: std::mem::size_of::<perf_event_attr>() as u32,
+                config: perf_hw_id_PERF_COUNT_HW_CPU_CYCLES as u64,
+                ..Default::default()
+            };
+            event.set_disabled(1);
+            event.set_exclude_kernel(1);
+            event.set_exclude_hv(1);
+            let fd: isize;
+            fd = perf_event_open(event, 0, -1, -1, 0);
+            let mut cnt: u64 = 0;
+            let buf: *mut libc::c_void = &mut cnt as *mut _ as *mut libc::c_void;
+            unsafe {
+                ioctl(fd as i32, ENABLE as u64, 0);
+                read(fd as i32, buf, std::mem::size_of_val(&cnt));
+            }
+            if cnt == 0 {
+                if settings.verbose {
+                    return TestResult::Failed(format!(
+                        "\nINFO\tThe CPU cycles count was {}. It should be bigger than that",
+                        cnt
+                    ));
+                }
+                return TestResult::Failed(format!("({})", cnt));
+            }
+            TestResult::Passed
+        }
+        Test {
+            name: "test_read_ioctl".to_string(),
+            description: "read / ioctl sanity check".to_string(),
+            call: check_read,
+            subtests: Vec::new(),
+            is_subtest: true,
+        }
+    }
+
     Test {
         name: "fd_sanity".to_string(),
         description: "File descriptor sanity tests".to_string(),
         call: dummy,
-        subtests: vec![test_perf_event_open()],
+        subtests: vec![test_perf_event_open(), test_read()],
         is_subtest: false,
     }
 }
